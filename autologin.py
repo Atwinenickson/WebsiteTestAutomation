296c6540dd3fed572b798278cf930353b1df42d1
Atwine Nickson <atwiinenicxon@gmail.com>
2022-09-07 11:29:43 +0300

    add signup function

--- autologin.py
+++ autologin.py
@@ -30,19 +30,16 @@ def phonelogin():
     url = environ.get('baseurl')
     driver.get(url)
 
-
-    print('trying to select')
-    driver.find_element('xpath', '/html/body/div[1]/div/section/header/div[2]/div[3]/div/div/div[2]/div/div/div[2]/button/span').click()
-
+    element = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/section/header/div[2]/div[3]/div/div/div[2]/div/div/div[2]/button/span"))
+    )
+    element.click()
     driver.implicitly_wait(100)
 
-    element = WebDriverWait(driver, 20).until(
+    element1 = WebDriverWait(driver, 20).until(
         EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div/div[2]/div/div/div[2]/div/div/div"))
     )
-    # driver.find_element('xpath', '/html/body/div[2]/div/div/div/div[2]/div/div/div[2]/div/div/div').click()
-    element.click()
-    
-    print('selected')
+    element1.click()
 
     driver.implicitly_wait(80)
     driver.find_element('xpath', '/html/body/div[2]/div[1]/div/div/div/div/div/div[2]/button').click()
@@ -76,18 +73,9 @@ def userlogin():
     element = WebDriverWait(driver, 20).until(
         EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div/div"))
     )
-    # driver.find_element('xpath', '/html/body/div[2]/div/div/div/div[2]/div/div/div[2]/div/div/div').click()
     element.click()
-    
-    print('selected')
 
     driver.implicitly_wait(80)
-
-
-
-    # driver.find_element('xpath', '/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div/div').click()
-
-    # print('selected')
 
 
     
@@ -96,21 +84,148 @@ def userlogin():
     )
     element2.send_keys(username)
 
-    # username_textbox = driver.find_element('xpath', '//*[@id="usernameUserInput"]')
-    # username_textbox.send_keys(username)
-
     element3 = WebDriverWait(driver, 20).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="password"]'))
     )
     element3.send_keys(password)
 
-    # password_textbox = driver.find_element('xpath', '//*[@id="password"]')
-    # password_textbox.send_keys(password)
-
     login_button = driver.find_element('xpath', '/html/body/div[2]/div[2]/div/div/div[3]/form/div[7]/div/button').click()
     login_button.submit()
     time.sleep(5)
 
-phonelogin()
+
+
+
+
+def signup():
+    driver = webdriver.Chrome("D:\Work\Others\EPORTAL\WEBDRIVERS\chromedriver.exe")
+    driver.maximize_window()
+
+    driver.implicitly_wait(120)
+    url = environ.get('baseurl')
+    driver.get(url)
+
+    register = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/section/header/div[2]/div[3]/div/div/div[2]/div/div/div[1]/button/span"))
+    )
+    register.click()
+
+    fName = 'Testing'
+    lName = 'Testing'
+    oName = 'Testing'
+    pNumber = '700988455'
+    uName = 'Shamlka2022'
+    uEmail = 'test@test.com'
+    cPassword = 'Test@2022An'
+    Password = 'Test@2022An'
+    
+
+    firstName = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="user-profile-data_firstName"]'))
+    )
+    firstName.send_keys(fName)
+
+
+    lastName = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="user-profile-data_lastName"]'))
+    )
+    lastName.send_keys(lName)
+
+    driver.implicitly_wait(80)
+
+
+    
+    otherName = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="user-profile-data_middleName"]'))
+    )
+    otherName.send_keys(oName)
+
+
+    phone = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="user-profile-data_phoneNum"]'))
+    )
+    phone.send_keys(pNumber)
+
+
+
+
+    userName = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="user-profile-data_username"]'))
+    )
+    userName.send_keys(uName)
+
+
+    email = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="user-profile-data_email"]'))
+    )
+    email.send_keys(uEmail)
+
+    driver.implicitly_wait(80)
+
+
+    
+    otherName = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="user-profile-data_middleName"]'))
+    )
+    otherName.send_keys(oName)
+
+    password = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="user-profile-data_password"]'))
+    )
+    password.send_keys(Password)
+
+    ConfirmPassword = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="user-profile-data_confirm"]'))
+    )
+    ConfirmPassword.send_keys(cPassword)
+
+    terms = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="user-profile-data_isTermsAndConditionsAccepted"]'))
+    )
+    terms.click()
+
+    time.sleep(2)
+
+    gender = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/section/main/div[1]/div/div[2]/form/div[1]/div[6]/div/div[1]/div/div[2]/div/div/div/label[1]/span[1]/input'))
+    )
+    gender.click()
+
+    nation = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="user-profile-data_userType"]'))
+    )
+    nation.click()
+
+    Snation = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div/div/div/div[2]/div[1]/div/div/div[1]/div'))
+    )
+    Snation.click()
+
+
+
+    
+
+
+    date = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="user-profile-data_dob"]'))
+    )
+    date.click()
+
+    selectdate = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div/div/div/div/div[1]/div[2]/table/tbody/tr[2]/td[4]/div'))
+    )
+    selectdate.click()
+
+
+
+
+    register = driver.find_element('xpath', '/html/body/div[2]/div[2]/div/div/div[3]/form/div[7]/div/button').click()
+    register.submit()
+    time.sleep(5)
+
+
+
+# phonelogin()
 # userlogin()
+signup()
 driver.quit()
